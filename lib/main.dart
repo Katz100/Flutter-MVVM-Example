c8a328fa79884590300b1cc28c190f579628c9bf
Katz100 <cody.hopkins108@gmail.com>
2026-05-03 10:26:41 -0700

    remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:test_app/home/home_viewmodel.dart';
-
-import 'home/home_screen.dart';
 import 'routes.dart';
 
 void main() {
